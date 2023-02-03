ef3edb3e30ae1f7d32b64048f6e5eaa70ce026c7
Ritom Gupta <ritomgupta99@gmail.com>
2023-02-03 00:28:52 -0800

    add gradient animation, not releasing hover sound yet

--- share.js
+++ share.js
@@ -1,19 +1,39 @@
 const shareButton = document.querySelector('.share-button');
 
-if(!navigator.share){
+if (!navigator.share) {
     shareButton.style.display = 'none';
 }
 
 shareButton.addEventListener('click', async () => {
     try {
         await navigator.share({
-            title:'Ritom',
-            url:'https://rittmang.xyz', 
-            text:'Just checked out Ritom\'s profile, you should take a look too :'
+            title: 'Ritom',
+            url: 'https://rittmang.xyz',
+            text: 'Just checked out Ritom\'s profile, you should take a look too :'
         });
         console.log("Shared");
     }
-    catch(err){
+    catch (err) {
         console.log("Error");
     }
 });
+
+
+//-------------------------
+
+// const audio = document.getElementById("hoverSound");
+// // const links = document.getElementsByClassName("link"); <- already used in script outside
+
+// for(const link of links){
+//     link.addEventListener("mouseenter", function () {
+//         audio.currentTime = 0;
+//         audio.play();
+//         link.style.border = "dashed var(--color) 2px;"
+//         // audio.loop = true;
+//     });
+    
+//     link.addEventListener("mouseleave",function(){
+//         audio.pause();
+//     });
+// }
+
